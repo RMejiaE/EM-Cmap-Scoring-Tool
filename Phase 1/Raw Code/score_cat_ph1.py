c45d7d04e9c166c7f832d8fa4332cde9d1124488
RMejiaE <78668372+RMejiaE@users.noreply.github.com>
2023-03-02 14:04:38 -0500

    Add files via upload

---
+++ Phase 1/Raw Code/score_cat_ph1.py
@@ -0,0 +1,24 @@
+import csv
+import os
+
+
+def fExt_simpleprintcat(vExtS_rootconcept,vExtL_inputfiles,vExtS_outputfile):
+
+    vLocL_mainheader=['Method','Root concept','Result file path','Result file name','Input files path']
+    vLocS_outputpath=os.path.dirname(vExtS_outputfile)
+    vLocS_outputname=os.path.basename(vExtS_outputfile)
+    vLocS_inputpath=os.path.dirname(vExtL_inputfiles[0])
+    vLocL_maindata=['Categorical',vExtS_rootconcept,vLocS_outputpath,vLocS_outputname,vLocS_inputpath]
+    vLocL_inputnames=[]
+
+    for file in vExtL_inputfiles:
+        vLocL_inputnames.append(os.path.basename(file))  # extract all the base names of the files.  
+    
+    with open(vExtS_outputfile,'w', newline='') as csvfile:
+         oExt_outputfile=csv.writer(csvfile,delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
+         oExt_outputfile.writerow(vLocL_mainheader)
+         oExt_outputfile.writerow(vLocL_maindata)
+         oExt_outputfile.writerow(['Filenames'])
+
+         for name in vLocL_inputnames:
+             oExt_outputfile.writerow([name]) #square brackets are used to put name in a sequence    
