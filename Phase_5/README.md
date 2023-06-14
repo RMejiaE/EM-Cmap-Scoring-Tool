eeb30711b9f43043d15993e5bd57c3e63b779e71
Eduardo RM <rodriguez.mejia.e@gmail.com>
2023-06-13 21:31:47 -0400

    Add existing file

---
+++ Phase_5/README.md
@@ -0,0 +1,47 @@
+# :open_file_folder: Phase 3 & 4
+For naming protocol within the code, please refer to the [Naming Protocol](https://github.com/RMejiaE/EM-Cmap-Scoring-Tool/blob/main/Naming%20protocol.md) documentation.
+## :wrench: Installing EM Cmap Scoring Tool
+- Please go over and click the [**EM_Cmap_Scoring_Tool_Launcher.zip**](https://github.com/RMejiaE/EM-Cmap-Scoring-Tool/blob/main/Phase_3_4/EM_Cmap_Scoring_Tool_Launcher.zip) at the top of this page, then clik on the *Download* option on the right side of the screen.
+- Under your computer folder, right-click **EM_Cmap_Scoring_Tool_Launcher.zip** and select *Extract all*.
+  - :warning: Please do not go diectly into the .zip folder
+  
+| ![imagen](https://user-images.githubusercontent.com/78668372/233404342-ba3c8d10-e2c7-437e-a0da-82f20dab5c04.png) |
+| :-: |
+| Figure 1: .zip extraction procedure |
+
+- Open the resuting folder after the extraction and double click on the **Cmap_Scoring_Tool_Launcher** application file to run the program (Figure 2).
+
+| ![imagen](https://user-images.githubusercontent.com/78668372/230093790-764e3e18-1c3c-4f5b-88ff-1b8e95e21c47.png) |
+| :-: |
+| Figure 2: Launch program |
+
+- If Windows alerts you because of the application beign unknown, please click on *Run anyway* or *More information* and then *Run anyway*
+
+| <img src="https://user-images.githubusercontent.com/78668372/229847812-d8e15832-8819-401c-af6d-07d6c938bb0a.png" width=50% height=60%> |
+| :-: |
+| Figure 3: Protection window |
+
+For future developers, the [Raw Code](https://github.com/RMejiaE/EM-Cmap-Scoring-Tool/tree/main/Phase_3_4/Raw%20code) folder contains the originl .py files of this phase. For editing the raw code, please donwload the files on the [Raw Code](https://github.com/RMejiaE/EM-Cmap-Scoring-Tool/tree/main/Phase_3_4/Raw%20code) folder into your computer via right-click -> *Save link as*.
+
+## :bar_chart: Wordbank update
+The Wordbank file was updated to have only one concept per cell for the test performed.
+The updated version can be seen in [Wordbank](https://github.com/RMejiaE/EM-Cmap-Scoring-Tool/blob/main/Phase_3_4/WordBank.csv) under this phase folder.
+### Traditional
+The number of concepts (NC) is obtained by counting all the concepts, excluding the root concept, present in the Cmap. The number of hierarchies (NH) is obtained by identifying the concepts that are connected directly to the root conept. The highest hierarchy (HH) corresponds to the hierarchy with the longest level. Lastly, the number of cross links (NCL) is obtained by counting the connections between paired concepts that are from different hierarchies.
+
+| ![imagen](https://user-images.githubusercontent.com/78668372/229846688-053cee04-0534-417b-a71f-7421fccae00b.png) | ![imagen](https://user-images.githubusercontent.com/78668372/229847229-deb70aed-940f-49b2-89c1-6a0e4ce86f47.png)    | ![imagen](https://user-images.githubusercontent.com/78668372/229847398-aec3cc25-ae7f-4eb0-b5ba-2de266ecda3a.png) |
+| :-: | :-: | :-: |
+| Figure 4: EM Cmap example with number of concepts (NC) identification. | Figure 5: Number of hierarchies (NH) and highest hierarcy (HH) identification. | Figure 6: Number of crosslinks (NCL) identification. |
+
+Figure 4 shows an example of a concept map with 9 concepts. After listing them, hierarchies and its concepts are identifyed (Figure 5). The subindex after the dot serves here to illustrate the level of each hierarchy and hence, the longest one. Finally crosslinks are identified (Figure 6).
+For this example, the NC will correspond to 9, NH is 3, the HH will be hierarchy 2 with 3 levels, and NCL value corresponds to 1.
+
+### Categorical
+The number of concepts (NC) is obtained by counting all the concepts, excluding the root concept, present in the Cmap. The number of categories (NCAT) is obtained by classifying the concepts present into each of the categories and counting the ones that contain at least one concept. For the case of Entrepreneurial Mindset (EM), the cateogries were defined by the [Word Bank](https://github.com/RMejiaE/EM-Cmap-Scoring-Tool/blob/main/Phase_2/WordBank.csv) developed by the research team members. Lastly, the number of interlinks (NIL) is obtained by counting the connections between paired concepts that are from different categories.
+
+| ![imagen](https://user-images.githubusercontent.com/78668372/229618016-94668494-1f69-418b-9535-5520c98fda32.png) | ![imagen](https://user-images.githubusercontent.com/78668372/229618225-0650527c-9952-4f7c-af26-fb7dd75c95c5.png) | ![imagen](https://user-images.githubusercontent.com/78668372/229618261-4721051f-92f2-4f9b-9fdc-54e18428638a.png) |
+| :-: | :-: | :-: |
+| Figure 7: EM Cmap example with number of concepts (NC) identification. | Figure 8: Number of categories (NCAT) identification. | Figure 9: Number of interlinks (NIL) identification. |
+
+Figure 7 shows an example of a concept map with 9 concepts. After listing the concepts, they are categorized using a word bank (Figure 8) and finally interlinks are identified (Figure 9).
+For this example, the NC will correspond to 9, NCAT will be 3, and NIL value corresponds to 2.
---
+++ Phase_5/Raw code/score_trad_ph5.py
@@ -0,0 +1,218 @@
+import re
+import csv
+import os
+import networkx as nx
+
+
+def fLoc_dataextraction(oGlo_file):
+    
+    #___________variables list_______________#
+    vLocL_idandconcept = []
+    vLocD_conceptsdict = {}
+    vLocL_idandlink = []
+    vLocD_linksdict = {}
+    vLocM_connectionsids = []
+    vLocM_toconcept = []
+    vLocM_fromconcept = []
+
+    #__________global variables______________#
+    global vGloM_conceptslinked 
+    global vGloL_concepts     
+    
+    vGloL_concepts=[]
+    vGloM_conceptslinked=[]
+    
+    #____looks within each line of the file for the concepts, linking phrases and connections__________#
+    for line in oGlo_file:
+        if "concept id=" in line:
+              vLocL_idandconcept = re.findall (r'"([^"]*)"', line)
+              vLocD_conceptsdict[vLocL_idandconcept[0]] = vLocL_idandconcept[1]
+              vLocL_idandconcept[1]=vLocL_idandconcept[1].replace('&#xa;', ' ')  #eliminates the line feed character
+              vLocL_idandconcept[1]=" ".join(vLocL_idandconcept[1].split())   #eliminates leading,ending and duplicated spaces 
+              vGloL_concepts.append(vLocL_idandconcept[1].lower())   #extracts the concepts
+              
+        if "linking-phrase id=" in line:
+              vLocL_idandlink = re.findall (r'"([^"]*)"', line)
+              vLocD_linksdict[vLocL_idandlink[0]] = vLocL_idandlink[1]
+
+        if "connection id=" in line:
+              vLocM_connectionsids.append(re.findall (r'"([^"]*)"', line))
+
+    #finds all the connections for a linking phrase   
+    for key in vLocD_linksdict:
+        vLocM_toconcept = []
+        vLocM_fromconcept = []
+        for connection in vLocM_connectionsids:
+
+             # if the linkphrase id is in the second position of the connectionsids, it is connecting to a concept
+             if key == connection[1]: 
+                  vLocD_conceptsdict[connection[2]]=vLocD_conceptsdict[connection[2]].replace('&#xa;', ' ')  #eliminates the line feed character
+                  vLocD_conceptsdict[connection[2]]=" ".join(vLocD_conceptsdict[connection[2]].split())  #eliminates leading,ending and duplicated spaces 
+                  vLocM_toconcept.append (vLocD_conceptsdict[connection[2]].lower())
+                  
+                  
+             # if the linkphrase id in the third position of the connectionsids, it is connecting from a concept       
+             if key == connection[2]: 
+                 vLocD_conceptsdict[connection[1]]=vLocD_conceptsdict[connection[1]].replace('&#xa;', ' ')  #eliminates the line feed character
+                 vLocD_conceptsdict[connection[1]]=" ".join(vLocD_conceptsdict[connection[1]].split())  #eliminates leading,ending and duplicated spaces 
+                 vLocM_fromconcept.append (vLocD_conceptsdict[connection[1]].lower())
+                
+          
+        for fromconcept in vLocM_fromconcept:   #makes all the possible pair combinations
+             for  toconcept in vLocM_toconcept:
+                vGloM_conceptslinked.append ([fromconcept, toconcept])
+
+    #Check that concepts have at least one connection.
+    for pos, concept in enumerate(vGloL_concepts):
+        if not any(concept in sub for sub in vGloM_conceptslinked):
+            vGloL_concepts.pop(pos)   #removes unconnected concepts
+
+    return
+
+def fExt_tradscoring(vExtS_rootconcept,vExtL_inputfiles,vExtS_outputfile):
+
+    #__________________Variables list______________________#
+    
+    vLocL_mainheader=['Method', 'Root concept', 'Result file path', 'Result file name', 'Input files path']
+    vLocS_outputpath=os.path.dirname(vExtS_outputfile)
+    vLocS_outputname=os.path.basename(vExtS_outputfile)
+    vLocS_inputpath=os.path.dirname(vExtL_inputfiles[0])
+    vLocL_maindata=['Traditional', vExtS_rootconcept,vLocS_outputpath, vLocS_outputname, vLocS_inputpath]
+    vLocL_inputnames=[]
+    global oGlo_file
+    vLocL_hierarchies=[]
+    vLocI_numconcepts=0
+    vLocI_numhierarchies=0
+    vLocI_highesthier=0
+    vLocI_numcrosslinks=0
+    vLocI_score=0
+
+    #________________Extraction of all file names__________#
+    for file in vExtL_inputfiles:
+        vLocL_inputnames.append(os.path.basename(file))    
+
+    #__________Opens or creates the result .csv file and writes the headers_________#
+    
+    try:
+        csvfile=open(vExtS_outputfile,'w', newline='')
+    except:
+        return -1
+
+    oLoc_outputfile=csv.writer(csvfile,delimiter=',',quoting=csv.QUOTE_MINIMAL)
+    oLoc_outputfile.writerow(vLocL_mainheader)
+    oLoc_outputfile.writerow(vLocL_maindata)
+
+    
+    #_________Iterates over each cmap file and does the scoring____________________#     
+    for index, cmapfile in enumerate(vExtL_inputfiles):
+        #____________Variables list____________________________#
+        vLocL_paths=[]
+        G = nx.MultiDiGraph()  #empty Multi-directed graph
+        
+        #opens the cmap file
+        oGlo_cmapfile = open(cmapfile)        
+
+        #verifies that file extension is correct .cxl
+        if os.path.splitext(vLocL_inputnames[index])[1][1:] == "cxl":
+
+            #calls the function that extracts the data from the file
+            fLoc_dataextraction(oGlo_cmapfile)
+            # close up the cmap file
+            oGlo_cmapfile.close()
+
+            #Checking that concepts and concepts link are not empty
+            if not vGloL_concepts or not vGloM_conceptslinked:
+                oLoc_outputfile.writerow([''])
+                oLoc_outputfile.writerow(['Filename', vLocL_inputnames[index],'No concepts and/or links between concepts found'])
+                continue
+
+            #Adds all edges from the list of concepts linked 
+            G.add_edges_from(vGloM_conceptslinked)
+
+            #verifies that root concept is in the graph
+            if vExtS_rootconcept.lower() not in G:
+                oLoc_outputfile.writerow([''])
+                oLoc_outputfile.writerow(['Filename', vLocL_inputnames[index], vExtS_rootconcept+' is not a concept in the map'])
+                continue
+
+            #____________# of concepts, hierachies, # of hierarchies______________#
+            
+            vLocI_numconcepts=G.number_of_nodes()-1  
+            vLocL_hierarchies=[x for x in map(str,G.successors(vExtS_rootconcept.lower()))] #to have a list instead of an iterator
+            vLocI_numhierarchies=G.out_degree(vExtS_rootconcept.lower())
+
+            
+            #______________________ Concepts in hierachies and Highest hierarchy ________________________#
+
+            #Iterates over the nodes in the graph and include a hierarchie attribute in zero
+            for n in G:
+                G.nodes[n]['hierarchy']=0
+
+            #Iterates over the first level nodes and assigns to them an unique number 
+            vLocI_numhierarchy=1
+            for h in vLocL_hierarchies:
+               G.nodes[h]['hierarchy'] = vLocI_numhierarchy
+               vLocI_numhierarchy += 1
+
+              
+            #iterates over the rest of the nodes, calculates to which first-level concept the path
+            #is shorter and assigns it to the corresponding hierarchy
+            vLocI_highesthier=0   
+            for n in G.nodes():
+               shortestpath = 0
+               assochier = ''
+               if n not in vLocL_hierarchies: 
+                   pathlist = []
+                   for h in vLocL_hierarchies:
+                       if nx.has_path(G, h, n):
+                           pathlist = nx.shortest_path(G, h, n)
+                           if shortestpath == 0:          #first iteration for n
+                               assochier = h
+                               shortestpath = len(pathlist)
+                                                                                                 
+                           else:                           #following iterations for n
+                               if (len(pathlist) < shortestpath):
+                                   assochier = h
+                                   shortestpath = len(pathlist)
+                                        
+                   if assochier:
+                       G.nodes[n]['hierarchy'] = G.nodes[assochier]['hierarchy']
+                       #loose nodes will keep a hierachy of 0
+
+                       if(len(pathlist)>vLocI_highesthier):
+                           vLocI_highesthier=len(pathlist)  #updates highest hierarchy
+
+                               
+            #______________________ Crosslinks ________________________#
+
+            #it looks for edges that have nodes from different hierachies
+            vLocI_numcrosslinks=0
+            for e in G.edges():
+                if (G.nodes[e[0]]['hierarchy']!=0) and (G.nodes[e[1]]['hierarchy']!=0):
+                   if G.nodes[e[0]]['hierarchy']!= G.nodes[e[1]]['hierarchy']:
+                       vLocI_numcrosslinks+=1
+
+            #_______________________ score _____________________________#
+            #Score = (NC) + (HH)*5 + (NCL)*10
+        
+            vLocI_score=vLocI_numconcepts + vLocI_highesthier*5 + vLocI_numcrosslinks*10
+                       
+            #writes the file name
+            oLoc_outputfile.writerow([''])
+            oLoc_outputfile.writerow(['Filename', vLocL_inputnames[index]])
+            oLoc_outputfile.writerow(['# of concepts', vLocI_numconcepts])
+            oLoc_outputfile.writerow(['Hierachies', vLocL_hierarchies])
+            oLoc_outputfile.writerow(['# of hierarchies', vLocI_numhierarchies])
+            oLoc_outputfile.writerow(['Highest hierarchy', vLocI_highesthier])
+            oLoc_outputfile.writerow(['# of crosslinks', vLocI_numcrosslinks])
+            oLoc_outputfile.writerow(['Traditional score', vLocI_score, 'NC+(HH*5)+(NCL*10)'])
+
+
+        #if file extension is not .cxl, writes and continues 
+        else:
+            oLoc_outputfile.writerow([''])
+            oLoc_outputfile.writerow(['Filename', vLocL_inputnames[index],'Incorrect file extension'])
+                     
+            continue
+        
+    csvfile.close()
